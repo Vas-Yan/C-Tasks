7ba0ae200aff5e650481eb713583e82673d9ab74
Vasily Yanichkin <vas-yan2007@yandex.ru>
2021-11-07 20:35:10 +0300

    Task00 addional solution added

--- Task00/Program.cs
+++ Task00/Program.cs
@@ -5,5 +5,10 @@ int APowerTwo = 0;
 APowerTwo=a*a;
 Console.WriteLine(APowerTwo);
 
+// на семинаре предложено такое решение
+/* double a;
+string s = Console.ReadLine();
+a=Convert.ToInt32(s);
+Console.WriteLine(Math.Pow(a,2)); */
 
 
