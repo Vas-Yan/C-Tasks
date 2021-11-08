417c962c0df8ab101b1821fe8e102002ee641b29
Vasily Yanichkin <vas-yan2007@yandex.ru>
2021-11-08 16:36:26 +0300

    Task02 added

--- Task00/Program.cs
+++ Task00/Program.cs
@@ -1,14 +1,14 @@
 ﻿// Вывести квадрат числа
-/* int a = new Random().Next (0,10);
+int a = new Random().Next (0,10);
 Console.WriteLine(a);
 int APowerTwo = 0;
 APowerTwo=a*a;
-Console.WriteLine(APowerTwo); */
+Console.WriteLine(APowerTwo);
 
 // на семинаре предложено такое решение
-double a;
+/* double a;
 string s = Console.ReadLine();
 a=Convert.ToInt32(s);
-Console.WriteLine(Math.Pow(a,2));
+Console.WriteLine(Math.Pow(a,2)); */
 
 
---
+++ Task02/Program.cs
@@ -0,0 +1,25 @@
+﻿// Даны два числа. Показать большее и меньшее.
+int GetRandom (int minValue, int maxValue)
+{
+    return new Random().Next(minValue, maxValue);
+}
+int [] A = new int [2];
+int index = 0;
+while (index<A.Length) 
+   {
+       A[index]= GetRandom(0,100);
+       index++;
+   } 
+index=0;
+while (index<A.Length) 
+{
+    Console.WriteLine(A[index]);
+    index++;
+}
+int max = A.Max();
+ Console.WriteLine();
+ Console.Write("max = "+max);
+
+ int min = A.Min();
+ Console.WriteLine();
+ Console.Write("min = "+ min);
