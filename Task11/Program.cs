72245db5bea1b0aad34e8f57ffed18852b26aafa
Vasily Yanichkin <vas-yan2007@yandex.ru>
2021-11-08 19:45:19 +0300

    Task11 added

---
+++ Task11/Program.cs
@@ -0,0 +1,18 @@
+﻿// Дано число из отрезка [10, 99]. Показать наибольшую цифру числа
+
+static int getTheHighestDigit (int a)
+{int max = 0;
+while (a !=0)
+    {
+        if(a%10>max) max = a % 10;
+        a/=10;
+    }
+return max;
+}
+int a = new Random().Next(10,99);
+Console.WriteLine();
+Console.Write(a+"<-- число");
+
+
+Console.WriteLine();
+Console.Write(getTheHighestDigit(a)+"<-- наибольшая цифра числа");
