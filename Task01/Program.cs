0a11ce8cab21ad46dadaa18daaabde2cca97ee43
Vasily Yanichkin <vas-yan2007@yandex.ru>
2021-11-08 09:41:35 +0300

    Task 02 solved

---
+++ Task01/Program.cs
@@ -0,0 +1,15 @@
+﻿// По двум заданным числам проверять, является ли первое квадратом второго
+double a;
+string s = Console.ReadLine();
+a= Convert.ToInt32(s);
+double b;
+string d = Console.ReadLine();
+b = Convert.ToInt32 (d);
+if(Math.Pow(b,2)==a)
+{
+    Console.Write("Yes");
+}
+if((Math.Pow(b,2)!=a))  
+{
+    Console.Write("No");
+    }
