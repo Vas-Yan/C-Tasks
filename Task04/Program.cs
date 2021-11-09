3986d81655b2462848603284c89a7a9ad10efbab
Vasily Yanichkin <vas-yan2007@yandex.ru>
2021-11-09 13:39:49 +0300

    Task04 added

---
+++ Task04/Program.cs
@@ -0,0 +1,29 @@
+﻿// Найти максимальное из трех чисел
+int GetRandom (int minValue, int maxValue)
+{
+    return new Random().Next(minValue,maxValue);
+}
+int [] A  = new int [3];
+int index = 0;
+while(index<A.Length)
+{
+    A[index]=GetRandom(-1,999);
+    index++;
+}
+index = 0;
+while(index<A.Length)
+{
+    Console.WriteLine(A[index]);
+    index++;
+}
+index=0;
+int max = 0;
+while (index<A.Length)
+{
+    max=A.Max();
+    index++;
+}
+index=0;
+Console.WriteLine();
+Console.WriteLine(max);
+
