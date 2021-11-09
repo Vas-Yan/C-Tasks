888a1b9d1955e97922ae623eacb244c13575fcba
Vasily Yanichkin <vas-yan2007@yandex.ru>
2021-11-09 13:11:32 +0300

    Task03 added

---
+++ Task03/Program.cs
@@ -0,0 +1,31 @@
+﻿//По заданному номеру дня недели вывести его название
+Console.Write("Введите номер дня недели (1-7) ->");
+string s = Console.ReadLine();
+switch (s)
+{
+
+    case "1":
+    Console.WriteLine("Понедельник");
+    break;
+    case "2":
+    Console.WriteLine("Вторник");
+    break;
+    case "3":
+    Console.WriteLine("Среда");
+    break;
+    case "4":
+    Console.WriteLine("Четверг");
+    break;
+    case "5":
+    Console.WriteLine("Пятница");
+    break;
+    case "6":
+    Console.WriteLine("Суббота");
+    break;
+    case "7":
+    Console.WriteLine("Воскресение");
+    break;
+    default:
+    Console.WriteLine("Error");
+    break;
+}
--- Task11/Program.cs
+++ Task11/Program.cs
@@ -1,11 +1,11 @@
 ﻿// Дано число из отрезка [10, 99]. Показать наибольшую цифру числа
 
-static int getTheHighestDigit (int a)
+int getTheHighestDigit (int a)
 {int max = 0;
 while (a !=0)
     {
         if(a%10>max) max = a % 10;
-        a/=10;
+        a=a/10;
     }
 return max;
 }
