2321a30eb17986c597ed397b010de4b276419d83
Vasily Yanichkin <vas-yan2007@yandex.ru>
2021-11-10 20:18:46 +0300

    Task06 added

---
+++ Task06/Program.cs
@@ -0,0 +1,18 @@
+﻿// Выяснить является ли число чётным
+int GetRandomNumber (int minValue, int maxValue)
+{
+    return new Random().Next(minValue, maxValue);
+}
+int a = GetRandomNumber(-100,999);
+
+Console.WriteLine(a); // <- выводим на консоль число, чтобы можно было проверить
+if (a % 2 == 0) 
+{
+    Console.Write("число является четным");
+}
+    else
+    {
+        Console.Write("число является нечетным");
+
+    }
+
