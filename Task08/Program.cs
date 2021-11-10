7dd86b42c2070ed86841c89ee2df463286ce6465
Vasily Yanichkin <vas-yan2007@yandex.ru>
2021-11-10 21:58:58 +0300

    Task08 solution added

---
+++ Task08/Program.cs
@@ -0,0 +1,25 @@
+﻿// Показать четные числа от 1 до N
+
+/* Console.WriteLine("Введите число >1");
+int b;
+b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Четные числа от 1 до N");
+ 
+ int i=1;
+ while (i< b)
+ {
+     if (i%2 == 0)
+     {
+         Console.Write(i+ "; ");
+    
+     }
+      i++;
+ } */
+
+Console.WriteLine("введите число>1");
+int b = Convert.ToInt32(Console.ReadLine());
+int i = 0;
+for (i=1; i<b+1;i++) if (i%2==0)
+{
+    Console.Write (i + "; ");
+}  
