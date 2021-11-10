505cfbd5c63c392fb40c5051ed64c1aaec6e1223
Vasily Yanichkin <vas-yan2007@yandex.ru>
2021-11-10 21:13:18 +0300

    Task07 solution added

---
+++ Task07/Program.cs
@@ -0,0 +1,8 @@
+﻿// Показать числа от -N до N
+
+Console.WriteLine ("введите отрицательное число");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите положительное число");
+int b = Convert.ToInt32(Console.ReadLine());
+int count = (-a) + b;
+for (int i =0; i<count+1; Console.Write(a+";"), a++, i++);
