84a6c9523af5050b85a5c7488337237e0a67927d
Vasily Yanichkin <vas-yan2007@yandex.ru>
2021-11-11 19:06:35 +0300

    Task15 solution added

---
+++ Task15/Program.cs
@@ -0,0 +1,11 @@
+﻿// Дано число. Проверить кратно ли оно 7 и 23
+int a = new Random().Next(1,999);
+Console.WriteLine(a);
+if(a%7==0 && a%23==0)
+{
+    Console.WriteLine("multiple of 7 and 23");
+}
+else
+{
+    Console.WriteLine("NOT a multiple of 7 and 23");
+}
