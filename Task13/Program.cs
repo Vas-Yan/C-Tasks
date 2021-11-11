f2720e9092ea2c9542603086d2156e58be4cfd11
Vasily Yanichkin <vas-yan2007@yandex.ru>
2021-11-11 18:47:11 +0300

    Task13 Solution added

---
+++ Task13/Program.cs
@@ -0,0 +1,17 @@
+﻿// Выяснить, кратно ли число заданному, если нет, вывести остаток.
+int A = new Random().Next(11,999);
+Console.WriteLine(A);
+Console.WriteLine();
+int B = new Random().Next(1,10);
+Console.WriteLine(B);
+double Result = A/B;
+double Excess = A-Result*B;
+Console.WriteLine();
+if(A%B==0) 
+{
+    Console.WriteLine("число кратно заданному");
+}
+else
+{
+    Console.WriteLine(Excess+ " <-- excess from devision");
+}
