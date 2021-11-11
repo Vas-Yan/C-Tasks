729791fb00b0aab006b6fc28da158f25c865e3a5
Vasily Yanichkin <vas-yan2007@yandex.ru>
2021-11-11 17:06:29 +0300

    Task05 Solution added

---
+++ Task05/Program.cs
@@ -0,0 +1,10 @@
+﻿// Написать программу вычисления значения функции y=f(a)
+double F ( double  a)
+{
+   double y = Math.Pow(a,2) + 3*a-4;
+    return y;
+}
+double a = new Random().Next(1,10);
+Console.WriteLine(a);
+Console.WriteLine(F(a));
+
