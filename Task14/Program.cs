2a1bf1337e270e63c4f3eaaccdbd4ba92822108d
Vasily Yanichkin <vas-yan2007@yandex.ru>
2021-11-11 18:54:01 +0300

    Task14 Solution added

---
+++ Task14/Program.cs
@@ -0,0 +1,12 @@
+﻿// Найти третью цифру числа или сообщить, что её нет
+int a = new Random().Next(0,999);
+Console.WriteLine(a);
+int ThirdDigit = a%10;
+if(a>100)
+{
+    Console.WriteLine(ThirdDigit);
+}
+else
+{
+    Console.WriteLine("no 3d digit in the given number");
+}
