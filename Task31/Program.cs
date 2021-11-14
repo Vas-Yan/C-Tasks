06ddbab4b7382f840c95034bd1f36432fe857734
Vasily Yanichkin <vas-yan2007@yandex.ru>
2021-11-14 20:12:36 +0300

    Task31 solution added

---
+++ Task31/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задать массив из 8 элементов и вывести их на экран
+int GetRandom (int minValue, int maxValue)
+{
+    return new Random().Next(minValue, maxValue);
+}
+int [] A = new int [8];
+int index = 0;
+while (index<A.Length)
+{
+    A[index]= GetRandom(1,999);
+    index++;
+}
+index = 0;
+while(index<A.Length)
+{
+    Console.WriteLine(A[index]);
+    index++;
+}
