cedc658b01fb8b465f27d3438897975853eecd30
Vasily Yanichkin <vas-yan2007@yandex.ru>
2021-11-14 20:21:20 +0300

    Task32 solution added

---
+++ Task32/Program.cs
@@ -0,0 +1,5 @@
+﻿//  Задать массив из 8 элементов, заполненных нулями и единицами вывести их на экран 
+int [] A = new int [8];
+for(int i = 0; i<A.Length; Console.WriteLine(A [i]), i++);
+Console.WriteLine();
+for(int i = 0; i<A.Length; A[i] = 1, Console.WriteLine(A [i]), i++);
