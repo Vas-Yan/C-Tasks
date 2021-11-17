a04c0643169ed0cac66b3c9cd23c0b7d35b1ff15
Vasily Yanichkin <vas-yan2007@yandex.ru>
2021-11-17 21:21:00 +0300

    Task34 solution added

---
+++ Task34/Program.cs
@@ -0,0 +1,12 @@
+﻿// Написать программу замену элементов массива на противоположные
+int GetRandom (int minValue, int maxValue)
+{
+   return new Random().Next(minValue, maxValue);
+}
+int [] A = new int [10];
+int i =0;
+for (i =0; i<A.Length; A[i] = GetRandom(-100,100), Console.WriteLine(A[i]), i++);
+Console.WriteLine();
+int [] ANew = new int [10];
+i=0;
+for (int j=0; j<ANew.Length; ANew[j]= A[i]*(-1), Console.WriteLine(ANew[j]), j++, i++);
