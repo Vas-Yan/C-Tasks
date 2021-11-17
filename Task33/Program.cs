2a0d1f27e4733bff2baa20fe6504c6518d68b1f5
Vasily Yanichkin <vas-yan2007@yandex.ru>
2021-11-17 21:01:53 +0300

    Task33 solution added

---
+++ Task33/Program.cs
@@ -0,0 +1,15 @@
+﻿//Задать массив из 12 элементов, заполненных числами из [0,9]. 
+// Найти сумму положительных/отрицательных элементов массива
+int NewRandom (int minValue, int maxValue)
+{
+    return new Random().Next(minValue, maxValue);
+}
+int [] A = new int [12];
+int i = 0;
+for(i=0; i<A.Length; A[i]=NewRandom(0,9), Console.WriteLine (A [i]), i++);
+int sum = 0;
+for (i = 0; i <A.Length; i++)
+{
+    sum = sum + A[i];
+}
+Console.WriteLine($"\n {sum}");
