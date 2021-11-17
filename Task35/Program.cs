1f5160a4ab0866d3c7fc2efdb9c5fddfa3a28afd
Vasily Yanichkin <vas-yan2007@yandex.ru>
2021-11-17 22:20:20 +0300

    Task 35 added

---
+++ Task35/Program.cs
@@ -0,0 +1,30 @@
+﻿// Определить, присутствует ли в заданном массиве, некоторое число 
+int NewRandom (int minValue, int maxValue)
+{
+    return new Random().Next(minValue, maxValue);
+}
+int [] A = new int [12];
+void GetArray (int [] A)
+{
+    for (int i=0; i<A.Length; i++)
+    {
+        A[i]= NewRandom(-100,100);
+    Console.Write($"{A[i]}  ");
+    }
+}
+GetArray(A);
+Console.WriteLine();
+Console.WriteLine ("укажите число");
+int s = Convert.ToInt32(Console.ReadLine());
+string Find(int [] A)
+{
+    int count = A.Length;
+    string Result = "числа нет";
+    for (int i=0; i<count; i++)
+    {
+        if (A[i]==s) Result = "число есть";
+    }
+
+    return Result;
+}
+Console.WriteLine(Find (A)); 
