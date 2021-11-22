16e1e669e94975d45f1630af73962f6c67a2ecaf
Vasily Yanichkin <vas-yan2007@yandex.ru>
2021-11-22 21:17:00 +0300

    Task 38 solution added

---
+++ Task38/Program.cs
@@ -0,0 +1,18 @@
+﻿//  Найти сумму чисел одномерного массива стоящих на нечетной позиции
+int GetRandom (int minValue, int maxValue)
+{
+    return new Random().Next(minValue, maxValue);
+}
+int [] A = new int [10];
+for (int i=0; i<A.Length; A[i]=GetRandom(-1000,1000), Console.Write($"{A[i]}; "), i++);
+int SumOddNumbers (int [] A)
+{
+    int sum = 0;
+    for (int j=0; j<A.Length; j++)
+    {
+        if (j%2==0) sum = sum + A[j];
+    }
+    return sum;
+}
+Console.WriteLine();
+Console.WriteLine(SumOddNumbers(A));
