abfad518abb0a76a891df7b7bc06013cce0fa796
Vasily Yanichkin <vas-yan2007@yandex.ru>
2021-11-28 22:17:31 +0300

    Task 23 added

---
+++ Task23/Program.cs
@@ -0,0 +1,29 @@
+﻿// Показать таблицу квадратов чисел от 1 до N 
+int GetRandom (int minValue, int maxValue)
+{
+    return new Random().Next(minValue,maxValue);
+}
+void TableOfSquares (int N)
+{   
+    for (int j=1; j<=N; j++)
+    {
+        Console.WriteLine($"квадрат числа {j}={Math.Pow(j,2)}");
+    }
+}
+int N=GetRandom(1,100);
+Console.WriteLine($"число N -> {N}");
+TableOfSquares(N);
+
+/* for(int i=0; i<Array.Length; i++, ArrayConsole.WriteLine(Array[i]));
+
+double TableOfSquares (int [] Array)
+{
+    double result = 0;
+    for (int i = 0; i<Array.Length; i++)
+    {
+        result = Math.Pow (Array[i],2);
+        result ++;
+    }
+    return result;
+} 
+Console.WriteLine(TableOfSquares(Array)); */
