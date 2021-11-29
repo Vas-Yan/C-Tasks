46f9db51577c22411557aa3e621fcaab5c6ca90d
Vasily Yanichkin <vas-yan2007@yandex.ru>
2021-11-29 21:05:57 +0300

    Task24 added

---
+++ Task24/Program.cs
@@ -0,0 +1,13 @@
+﻿// Найти кубы чисел от 1 до N
+int GetRandom (int minValue, int maxValue)
+{
+    return new Random().Next(minValue,maxValue);
+}
+int N = GetRandom(1,10);
+Console.WriteLine(N);
+void TableOfCubes (int N)
+{
+    for (int i = 1; i<=N; i++)
+    Console.WriteLine($"куб числа {i} = {i*i*i}");
+}
+TableOfCubes(N);
