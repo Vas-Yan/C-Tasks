819f240cc381016e4834a212196420acbf60a118
Vasily Yanichkin <vas-yan2007@yandex.ru>
2021-11-29 21:28:59 +0300

    Task25 solution added

---
+++ Task25/Program.cs
@@ -0,0 +1,20 @@
+﻿// Найти сумму чисел от 1 до А
+int GetRandom (int minValue, int maxValue)
+{
+    return new Random().Next(minValue, maxValue);
+}
+int A = GetRandom(1,10);
+Console.WriteLine($"A = {A}");
+int SumOfNumbers(int A)
+{
+    int sum = 0;
+    for (int i = 1; i <= A; i++)
+    {
+       sum = sum + i;
+    }
+    Console.WriteLine($"сумма чисел от 1 до А = {sum}");
+    return sum;
+    
+}
+SumOfNumbers(A);
+//Console.WriteLine($"сумма чисел от 1 до А = {SumOfNumbers(A)}");
