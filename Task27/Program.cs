da64747988da2f11d0d2278f266d732362609703
Vasily Yanichkin <vas-yan2007@yandex.ru>
2021-11-30 21:40:56 +0300

    Task 27 added

---
+++ Task27/Program.cs
@@ -0,0 +1,12 @@
+﻿// Определить количество цифр в числе
+int CountDigit(int n)
+{
+    int k = 0;
+    while (n!=0)
+    {
+        k++;
+        n=n/10;
+    }
+    return k;
+}
+Console.WriteLine(CountDigit(1234));
