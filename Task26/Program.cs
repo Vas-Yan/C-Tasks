c01b59e45ac4e8b8857f11595fb3802cd641b9d6
Vasily Yanichkin <vas-yan2007@yandex.ru>
2021-11-30 22:06:10 +0300

    Task 26 added

---
+++ Task26/Program.cs
@@ -0,0 +1,20 @@
+﻿// Возведите число А в натуральную степень B используя цикл
+int GetRandom (int minValue, int maxValue)
+{
+    return new Random().Next(minValue,maxValue);
+}
+int A = GetRandom(1,10);
+Console.WriteLine ($"число А = {A}");
+int B = GetRandom (1,10);
+Console.WriteLine($"число B = {B}");
+double Exponentiation (int A)
+{
+    double result = 0;
+    for (int i=0; i<=B; i++)
+    {
+        result = Math.Pow(A,B);
+    }
+    return result;
+}
+
+Console.WriteLine($"число A в степени B = {Exponentiation(A)}");
