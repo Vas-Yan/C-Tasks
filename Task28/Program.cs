9fe4634b1b236619e02db80dd55291041317d544
Vasily Yanichkin <vas-yan2007@yandex.ru>
2021-12-01 21:11:59 +0300

    Task 28 added

---
+++ Task28/Program.cs
@@ -0,0 +1,17 @@
+﻿// Подсчитать сумму цифр в числе
+int GetRandomNumber (int MinValue, int MaxValue)
+{
+    return new Random().Next(MinValue,MaxValue);
+}
+int Number = GetRandomNumber (0,1000);
+Console.WriteLine(Number);
+int CountSumOfDigits (int Number)
+{
+    int FirstDigit = Number/100;
+    int SecondDigit = Number%100/10;
+    int ThirdDigit = Number%10;
+    int sum = FirstDigit + SecondDigit + ThirdDigit;
+    return sum;
+}
+CountSumOfDigits(Number);
+Console.WriteLine (CountSumOfDigits(Number));
