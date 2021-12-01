41b788400c32dc35af259c327d23a59d7bb063a7
Vasily Yanichkin <vas-yan2007@yandex.ru>
2021-12-01 21:34:33 +0300

    Task29 added

---
+++ Task29/Program.cs
@@ -0,0 +1,18 @@
+﻿// Написать программу вычисления произведения чисел от 1 до N
+int GetRandomNumber (int MinValue, int MaxValue)
+{
+    return new Random().Next(MinValue,MaxValue);
+}
+int N = GetRandomNumber (1,27);
+double Product (int N)
+{
+    double result = 1;
+    for (int i=1; i<=N; i++)
+    {
+        result = result*i;
+    }
+    return result;
+}
+Product(N);
+Console.WriteLine(N);
+Console.WriteLine(Product(N));
