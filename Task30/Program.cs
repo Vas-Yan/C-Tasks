891163f862ea5814614d66b4b1af04ed34bd6bfc
Vasily Yanichkin <vas-yan2007@yandex.ru>
2021-12-01 21:52:10 +0300

    Task30 added

---
+++ Task30/Program.cs
@@ -0,0 +1,18 @@
+﻿// Показать кубы чисел, заканчивающихся на четную цифру
+int GetRandomNumber   (int MinValue, int MaxValue)
+{
+    return new Random().Next(MinValue, MaxValue);
+}
+int N = GetRandomNumber(0,100);
+int [] Array = new int [N];
+void FillArray (int [] Array)
+{
+    for (int i=1; i<Array.Length; i++)
+    {
+        if(Math.Pow(i,3)%2==0)
+        {
+            Console.WriteLine($"куб числа {i} = {Math.Pow(i,3)}");
+        }
+    }
+}
+FillArray(Array);
