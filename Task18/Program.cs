12eaf2bbfa9b7874d0b2418f91df42024c860a36
Vasily Yanichkin <vas-yan2007@yandex.ru>
2021-12-02 21:33:17 +0300

    Task 18 added

---
+++ Task18/Program.cs
@@ -0,0 +1,39 @@
+﻿// Проверить истинность утверждения ¬(X ⋁ Y) = ¬X ⋀ ¬Y
+bool X = true;
+bool Y = false;
+bool FirstPart = !(X&&Y);
+bool SecondPart = !X||!Y;
+if(FirstPart == SecondPart)
+{
+    Console.WriteLine("True");
+}
+else
+{
+    Console.WriteLine("False");
+}
+
+
+
+
+
+
+
+
+
+
+/* int getTheHighestDigit (int a)
+{int max = 0;
+while (a !=0)
+    {
+        if(a%10>max) max = a % 10;
+        a=a/10;
+    }
+return max;
+}
+int a = new Random().Next(10,99);
+Console.WriteLine();
+Console.Write(a+"<-- число");
+
+
+Console.WriteLine();
+Console.Write(getTheHighestDigit(a)+"<-- наибольшая цифра числа"); */
