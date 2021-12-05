a827e1036ae02063afb22a02e3d1e58f14f28024
Vasily Yanichkin <vas-yan2007@yandex.ru>
2021-12-05 17:45:24 +0300

    Task19 added

---
+++ Task19/Program.cs
@@ -0,0 +1,16 @@
+﻿// Определить номер четверти плоскости, в которой находится точка с координатами Х и У, 
+// причем X ≠ 0 и Y ≠ 0
+string Shape (int X, int Y)
+{
+    string result = string.Empty;
+if(X>0&&Y>0) Console.WriteLine ("First quarter");
+if(X<0&&Y>0) Console.WriteLine ("Second quarter");
+if(X<0&&Y<0) Console.WriteLine ("Third quarter");
+if(X>0&&Y<0) Console.WriteLine ("Forth quarter");
+return result;
+}
+Console.WriteLine("insert number (-100,+100)");
+int X = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("insert number (-100,+100)");
+int Y = Convert.ToInt32(Console.ReadLine());
+Shape (X,Y);
