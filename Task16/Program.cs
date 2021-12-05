bc403b0e185e0538bf4484485f9a0b8f085b6502
Vasily Yanichkin <vas-yan2007@yandex.ru>
2021-12-05 17:16:35 +0300

    Task 16 added

---
+++ Task16/Program.cs
@@ -0,0 +1,16 @@
+﻿// Дано число обозначающее день недели. Выяснить является номер недели выходным днём
+int GetRandom (int MinValue, int MaxValue)
+{
+    return new Random ().Next(MinValue,MaxValue);
+}
+int Number = GetRandom(1,8);  
+Console.WriteLine(Number);
+void WeekdayOrWeekend (int Number)
+{
+    if(Number<6) Console.WriteLine("weekday"); // Monday-Friday correspond to numbers 1-5;
+    else
+    {
+        Console.WriteLine("weekend");//Saturday & Sunday - numbers 6-7
+    }
+}
+WeekdayOrWeekend(Number);
